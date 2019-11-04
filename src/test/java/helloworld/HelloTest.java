339370db494b87ec27fb146f7cfd564477763021
Sean C. Sullivan <github@seansullivan.com>
2019-11-04 17:20:54 -0500

    add [assertj] assertions

--- src/test/java/helloworld/HelloTest.java
+++ src/test/java/helloworld/HelloTest.java
@@ -1,8 +1,11 @@
 
 package helloworld;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import com.example.clients.petstore.definitions.User;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.concurrent.CompletionStage;
 import com.example.clients.petstore.user.GetUserByNameResponse;
 
@@ -10,6 +13,25 @@ public class HelloTest {
     @Test
     public void happyPath() {
         CompletionStage<GetUserByNameResponse> result = new Hello().execute();
-        System.out.println(result);
+        result.thenApply( response -> {
+            assertThat(result).isCompleted();
+            response.fold(this::handleOk, this::handleBadRequest, this::handleNotFound);
+            return result;
+        });
+    }
+    
+    private Object handleOk(User user) {
+        assertThat(user.getEmail()).isNotEmpty();
+        return user;
+    }
+
+    private Object handleBadRequest() {
+     fail("bad request");
+     return null;
+    }
+
+    private Object handleNotFound() {
+        fail("not found");
+        return null;
     }
 }
