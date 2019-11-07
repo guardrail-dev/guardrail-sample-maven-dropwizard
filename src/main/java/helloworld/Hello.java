2bfe8a6d7920766dca7f445f39ed8ea2e6d6cfe2
Sean C. Sullivan <github@seansullivan.com>
2019-11-06 21:11:42 -0500

    improve HelloTest

--- src/main/java/helloworld/Hello.java
+++ src/main/java/helloworld/Hello.java
@@ -1,13 +1,18 @@
 
 package helloworld;
 
+import com.example.clients.petstore.definitions.User;
 import com.example.clients.petstore.user.GetUserByNameResponse;
 import com.example.clients.petstore.user.UserClient;
 import java.util.concurrent.CompletionStage;
 
 public class Hello {
-  public CompletionStage<GetUserByNameResponse> execute() {
-      UserClient client = new UserClient.Builder().build();
-      return client.getUserByName("hello").call();
+  private UserClient client = new UserClient.Builder().build();
+
+  public CompletionStage<GetUserByNameResponse> createUser(final String username) {
+      String email = username + "@example.com";
+      User u = new User.Builder().withUsername(username).withEmail(email).build();
+      return client.createUser(u).call().thenCompose(
+              createUserResp -> client.getUserByName(username).call());
   }
 }
--- src/test/java/helloworld/HelloTest.java
+++ src/test/java/helloworld/HelloTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.fail;
 import com.example.clients.petstore.definitions.User;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -16,14 +16,16 @@ import com.example.clients.petstore.user.GetUserByNameResponse;
 public class HelloTest {
     @Test
     public void happyPath() throws Exception {
-        CompletableFuture<GetUserByNameResponse> future = new Hello().execute().toCompletableFuture();
+        final String username = randomUsername();
+        CompletableFuture<GetUserByNameResponse> future = new Hello().createUser(username).toCompletableFuture();
         GetUserByNameResponse response = future.get(10, TimeUnit.SECONDS);
         response.fold(this::handleOk, this::handleBadRequest, this::handleNotFound);
     }
     
     private Object handleOk(User user) {
         assertThat(user.getEmail().isPresent()).isTrue();
-        assertThat(user.getUsername()).isEqualTo(Optional.of("hello"));
+        assertThat(user.getUsername().isPresent()).isTrue();
+        assertThat(user.equals(user)).isTrue();
         return user;
     }
 
@@ -35,5 +37,9 @@ public class HelloTest {
     private Object handleNotFound() {
         fail("not found");
         return null;
+    }
+
+    private String randomUsername() {
+        return "user-" + UUID.randomUUID().toString();
     }
 }
