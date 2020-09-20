70cf767aed57859bb71222dca95cb57841965a48
Sean C. Sullivan <github@seansullivan.com>
2020-09-20 16:11:56 -0700

    add EqualsContractTest

---
+++ src/test/java/helloworld/EqualsContractTest.java
@@ -0,0 +1,38 @@
+package helloworld;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ScanResult;
+
+public class EqualsContractTest {
+
+    @Test
+    public void testEqualsContract() {
+        try (ScanResult scanResult =
+                     new ClassGraph()
+                             .enableClassInfo()
+                             .enableMethodInfo()
+                             .acceptPackages("com.example.clients.petstore.definitions")
+                             .scan()) {
+
+            assertThat(scanResult.getAllClasses().size()).isGreaterThan(0);
+            for (ClassInfo classInfo : scanResult.getAllClasses()) {
+                final boolean isBuilder = classInfo.getName().endsWith("Builder");
+                if (!isBuilder) {
+                  verify(classInfo.loadClass());
+                }
+            }
+        }        
+    }
+
+    private static void verify(final Class clazz) {
+        EqualsVerifier.forClass(clazz)
+                .usingGetClass()
+                .verify();
+    }
+}
